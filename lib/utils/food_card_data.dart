d42b0d9511bc69784ca5a938efe6f9818959e868
handuah <handuah@gmail.com>
2023-02-16 11:54:06 +0000

    added food card data data file

---
+++ lib/utils/food_card_data.dart
@@ -0,0 +1,37 @@
+List foodardData = [
+  {
+    'id': 1,
+    'foodImgUrl':
+        'https://thumbs.dreamstime.com/b/italian-pizza-white-background-isolated-italian-pizza-white-background-146709376.jpg',
+    'foodName': 'Meat Lovers Pizza',
+    'foodCost': 10.11
+  },
+  {
+    'id': 2,
+    'foodImgUrl':
+        'https://thumbs.dreamstime.com/b/veggie-burger-isolated-white-background-healthy-black-bean-veggie-burger-white-background-work-path-included-171567596.jpg',
+    'foodName': 'Veggie Burger',
+    'foodCost': 15.23
+  },
+  {
+    'id': 3,
+    'foodImgUrl':
+        'https://www.seekpng.com/png/small/270-2703250_menu-logo-fried-chicken-white-background.png',
+    'foodName': 'Chicken Wings',
+    'foodCost': 5.99
+  },
+  {
+    'id': 4,
+    'foodImgUrl':
+        'https://thumbs.dreamstime.com/b/rainbow-rotini-pasta-isolated-red-bowl-white-background-clipping-mask-39815083.jpg',
+    'foodName': 'Assorted Pasta',
+    'foodCost': 15.20
+  },
+  {
+    'id': 5,
+    'foodImgUrl':
+        'https://png.pngitem.com/pimgs/s/26-261285_clip-art-coca-cola-garrafa-png-coca-cola.png',
+    'foodName': 'Coca Cola',
+    'foodCost': 2.00
+  },
+];
